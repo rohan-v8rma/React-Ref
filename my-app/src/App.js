f7a2da08a90894ffb45d12119356f136c7c8983a
Rohan Verma <rv725555@gmail.com>
2023-01-11 20:28:26 +0530

    feat: Another way of passing props.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,17 @@
 import logo from './logo.svg';
 import AppStyles from './App.module.css';
 
-function App(props) {
+
+// Another way of passing props
+function App({lightthemebinary}) {
+// function App(props) {
   
   return (
     <div className={`${AppStyles.common} ` 
-    +  (props.lightthemebinary ? `${AppStyles.lighttheme}` : `${AppStyles.darktheme}`) }>
+    +  (lightthemebinary ? `${AppStyles.lighttheme}` : `${AppStyles.darktheme}`) }>
       hehehehehehehehhe
     </div>
   )
-  
-  // return ( 
-  // <div className={`${AppStyles.app}`}>
-  //   <header className={`${AppStyles.appheader}`}>
-  //     <img src={logo} className={`${AppStyles.applogo}`} alt="logo" />
-  //     <p>
-  //       Edit <code>src/App.js</code> and save to reload.
-  //     </p>
-  //     <a
-  //       className="App-link"
-  //       href="https://reactjs.org"
-  //       target="_blank"
-  //       rel="noopener noreferrer"
-  //     >
-  //       Learn React
-  //     </a>
-  //   </header>
-  // </div>
-  // )
 }
 
 export default App;
