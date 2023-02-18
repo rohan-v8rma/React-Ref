598d3befa515b2811e5e34157c1c37d983526457
Rohan Verma <rv725555@gmail.com>
2023-02-18 09:49:06 +0530

    chore: Renamed files.

--- my-app/src/Components/Functional-Stateful-Lifecycle-Comp.js
+++ my-app/src/Components/Functional-Stateful-Lifecycle-Comp.js
@@ -5,9 +5,16 @@ function FunctionalStatefulComp() {
     const [count, setCount] = useState(0);
     const [name, setName] = useState("John"); // The useState() hook can be used multiple times to manage multiple state variables.
 
+    // In the above cases, `setCount` and `setName` are state update functions.
+
     return (<div>
         <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment count</button>
+        {/* 
+        Arrow function (called Updater Function) has been used since the next state value is dependent on the current state value. 
+        
+        Read more about it here: https://beta.reactjs.org/reference/react/useState#updating-state-based-on-the-previous-state
+        */}
+        <button onClick={() => setCount(state => state + 1)}>Increment count</button>
 
         <p>Name: {name}</p>
         <button onClick={() => setName("Jane")}>Change name</button>
