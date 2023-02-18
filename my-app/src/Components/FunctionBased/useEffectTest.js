68f6fe29592d531a1795c6d2274b1e31edf54458
Rohan Verma <rv725555@gmail.com>
2023-02-18 20:31:03 +0530

    test: useEffect hook testing #2.

--- my-app/src/Components/FunctionBased/useEffectTest.js
+++ my-app/src/Components/FunctionBased/useEffectTest.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+//? NOTE: The double printing of the outputs of the callbacks passed to the useEffect function is due to React Strict Mode
+
 export default function UseEffectTest() {
     const [count, setCount] = useState(0)
 
@@ -11,7 +13,17 @@ export default function UseEffectTest() {
             .then(json => console.log(json))
     })
 
+    //! We can pass an array as the second argument of the useEffect hook which is called the `dependency array`. This can contain `state` or `prop` values, that trigger a re-render upon their change, and then subsequently result in a call to the callback passed to the `useEffect` hook. 
+    //! Observe that we are passing a callback to the useEffect hook, rather than a function call. So, the code will NOT necessarily execute synchronously on re-render.
 
+    //* We can make the execution of `useEffect` dependent on a couple of values, placed inside the `dependency array`
+    //? The code below achieves the function of only `componentDidMount`
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/2')
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }, []) // Empty dependency array
+ 
     return (<>
     {/* Passing a callback to the button element */}
     <button onClick={() => setCount(prevCount => prevCount + 1)}>bruhhhhhh{count}</button>
